04247b941810a93ff237ff80343649bbd88bd31b
Matsushoooo12 <matsush.oooo12@icloud.com>
2022-10-17 03:12:49 +0900

    タブ切り替え

--- src/screens/UserScreen.tsx
+++ src/screens/UserScreen.tsx
@@ -1,11 +1,35 @@
-import { View, Text } from "react-native";
+import { View, Text, useWindowDimensions } from "react-native";
 import React from "react";
+import { TabView, SceneMap } from "react-native-tab-view";
+
+const FirstRoute = () => (
+  <View style={{ flex: 1, backgroundColor: "#ff4081" }} />
+);
+
+const SecondRoute = () => (
+  <View style={{ flex: 1, backgroundColor: "#673ab7" }} />
+);
+
+const renderScene = SceneMap({
+  first: FirstRoute,
+  second: SecondRoute,
+});
 
 const UserScreen = () => {
+  const layout = useWindowDimensions();
+
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    { key: "first", title: "First" },
+    { key: "second", title: "Second" },
+  ]);
   return (
-    <View>
-      <Text>UserScreen</Text>
-    </View>
+    <TabView
+      navigationState={{ index, routes }}
+      renderScene={renderScene}
+      onIndexChange={setIndex}
+      initialLayout={{ width: layout.width }}
+    />
   );
 };
 
