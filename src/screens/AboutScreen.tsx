40b8e3dbbcd57e544d7c7467baed4fc2c6fadafd
Matsushoooo12 <matsush.oooo12@icloud.com>
2022-10-18 18:10:43 +0900

    fix

--- src/screens/AboutScreen.tsx
+++ src/screens/AboutScreen.tsx
@@ -1,5 +1,5 @@
-import { Box, Text, Button } from "native-base";
 import React from "react";
+import { Box, Text, Button } from "native-base";
 
 const AboutScreen = ({ navigation }: any) => {
   return (
--- src/screens/UserScreen.tsx
+++ src/screens/UserScreen.tsx
@@ -1,5 +1,5 @@
-import { View, Text, useWindowDimensions } from "react-native";
 import React from "react";
+import { View, useWindowDimensions } from "react-native";
 import { TabView, SceneMap } from "react-native-tab-view";
 
 const FirstRoute = () => (
