51874c6e1eeb29847f55594ccb8e2e4ae0593a1c
Matsushoooo12 <matsush.oooo12@icloud.com>
2022-10-18 18:11:14 +0900

    fix

--- App.tsx
+++ App.tsx
@@ -1,9 +1,5 @@
 import { NavigationContainer } from "@react-navigation/native";
-import HomeScreen from "./src/screens/HomeScreen";
-import UserScreen from "./src/screens/UserScreen";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Ionicons } from "@expo/vector-icons";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import StackNavigator from "./src/navigations/StackNavigator";
 import { NativeBaseProvider, Box } from "native-base";
 import theme from "./src/theme";
